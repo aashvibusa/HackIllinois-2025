36580406db0e535e947672bee0f27a0275f4e892
Rishi Mulchandani <rishimulchandani100@gmail.com>
2025-03-02 02:33:35 -0600

    deploy

--- app/app.py
+++ app/app.py
@@ -726,9 +726,8 @@ def place_order():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False)
-
-
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
